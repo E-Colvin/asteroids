c7215e0058f329373f4029ffbac7d97c45e8a4a4
E-Colvin <ezekielcolvin@fastmail.com>
2026-04-01 21:29:44 -0400

    add split logic

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
+from logger import log_event
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self,x ,y ,radius):
@@ -11,3 +13,20 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity*dt
+
+    def split(self):
+        self.kill()
+        if self.radius < ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            angle = random.uniform(20,50)
+            child_1 = self.spawn_child(angle)
+            child_2 = self.spawn_child(-angle)
+
+    def spawn_child(self,angle):
+        child_velocity = self.velocity.rotate(angle)
+        child_radius = self.radius - ASTEROID_MIN_RADIUS
+        child = Asteroid(self.position[0],self.position[1],child_radius)
+        child.velocity = child_velocity*1.2
+            
--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def main():
             for shot in shots:
                 if rock.collides_with(shot):
                     log_event("asteroid_shot")
-                    rock.kill()
+                    rock.split()
                     shot.kill()
 
         screen.fill("black")
